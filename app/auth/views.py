f20ae286c2730dc67c00d726f9b5529af913e237
expo180 <badersaadousalissou@gmail.com>
2023-11-08 01:00:46 +0100

    designfixes

--- app/auth/views.py
+++ app/auth/views.py
@@ -23,18 +23,7 @@ google = OAuth().remote_app(
     access_token_url='https://accounts.google.com/o/oauth2/token',
     authorize_url='https://accounts.google.com/o/oauth2/auth',
 )
-# Initialize Apple OAuth
-apple = OAuth().remote_app(
-    'apple',
-    consumer_key='',
-    consumer_secret='',
-    request_token_params={},
-    base_url='https://appleid.apple.com/auth/authorize',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://appleid.apple.com/auth/token',
-    authorize_url='https://appleid.apple.com/auth/authorize',
-)
+
 
 @auth.route('/apple_signup', methods=['GET'])
 def apple_signup():
